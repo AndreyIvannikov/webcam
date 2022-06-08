7c71f9f3f68455ea1635f105d56f16d5980cc96d
Andrey Ivannikov <dronxxx1993@gmail.com>
2022-06-08 21:44:20 +0400

    fixed

--- src/components/@ui/WDropdown/components/dropdown_list/index.vue
+++ src/components/@ui/WDropdown/components/dropdown_list/index.vue
@@ -1,5 +1,8 @@
 <template>
-    <div>
-        
-    </div>
+  <div>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam deserunt
+    ipsam nesciunt a inventore temporibus ratione velit dicta neque, consectetur
+    asperiores maxime, facere iste vitae modi quasi. Fugit, necessitatibus
+    explicabo.
+  </div>
 </template>
